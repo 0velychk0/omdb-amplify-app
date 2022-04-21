0883496034852384c255be4c0c10c9a05fe11aa8
Oleksandr_Velychko2 <Alex.Velichko@gmail.com>
2022-04-21 10:18:22 +0300

    Convert to a class

--- src/TableSearchModule.jsx
+++ src/TableSearchModule.jsx
@@ -1,38 +1,26 @@
-import React, { useState } from 'react';
-// import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 // const API_HOST = "http://localhost:3000";
 // const INVENTORY_API_URL = `${API_HOST}/TestData/data.json`;
 const IMDB_API_URL = "https://www.imdb.com/title/"
 const INVENTORY_API_URL = "https://www.omdbapi.com/?apikey=1ac1214b"
 
-let pageNumber = 1;
-let totalCount = 0;
+class HomeScreen extends React.Component {
 
-function App() {
-    const columns = [
-        { name: 'Poster', title: 'Poster' },
-        { name: 'Title', title: 'Title' },
-        { name: 'Year', title: 'Year' },
-        { name: 'imdbID', title: 'imdbID' },
-        { name: 'Type', title: 'Type' }
-    ];
-
-    const [data, setData] = useState([]);
+    constructor(props) {
+        super(props);
+        this.state = { data: [], pageNumber: 1, totalCount: 0 };
+    }
 
     // GET request function to your Mock API
-    const fetchInventory = (requiredPage) => {
-        pageNumber = requiredPage;
-        if (pageNumber < 1) pageNumber = 1;
-
-        console.log('fetchInventory called');
+    fetchInventory() {
         let textId = document.getElementById("textId").value.trim();
         let yearId = document.getElementById("yearId").value.trim();
 
-        console.log(`fetchInventory called with text: '${textId}', year: '${yearId}'`);
+        console.log(`fetchInventory called with text: '${textId}', year: '${yearId}', page: ${this.state.pageNumber}`);
 
         if (textId === "") {
-            setData([]);
+            this.setState({ data: [] });
             document.getElementById('resultSectionId').className = "hideme";
             return;
         }
@@ -40,82 +28,109 @@ function App() {
         if (yearId !== "") {
             uri = uri + "&y=" + yearId;
         }
-        if (pageNumber > 0) {
-            uri = uri + "&page=" + pageNumber;
+        if (this.state.pageNumber > 1) {
+            uri = uri + "&page=" + this.state.pageNumber;
         }
 
         fetch(`${INVENTORY_API_URL}${uri}`)
             .then(res => res.json())
             .then(json => {
-                totalCount = json.totalResults;
-                if (totalCount > 0)
+
+                this.setState({
+                    totalCount: json.totalResults,
+                    data: ((json.Search != null) ? json.Search : [])
+                });
+
+                if (json.totalResults > 0)
                     document.getElementById('resultSectionId').className = "";
-                else 
+                else
                     document.getElementById('resultSectionId').className = "hideme";
-                setData((json.Search != null) ? json.Search : [])
             })
             .catch(e => {
                 console.error("error:" + e);
                 document.getElementById('resultSectionId').className = "hideme";
-                setData([]);
+                this.setState({ data: [] });
             });
     }
 
-    // // Calling the function on component mount
-    // useEffect(() => {
-    //     console.log('useEffect called');
-    //     fetchInventory();
-    // }, []);
+    goNextPage() {
+        this.setState(prevState => ({
+            pageNumber: prevState.pageNumber + 1
+        }));
+        this.fetchInventory();
+    }
+    goPrevPage() {
+        this.setState(prevState => ({
+            pageNumber: ((prevState.pageNumber > 1) ? prevState.pageNumber - 1 : prevState.pageNumber)
+        }));
+        this.fetchInventory();
+    }
+    goFirstPage() {
+        this.setState(prevState => ({
+            pageNumber: 1
+        }));
+        this.fetchInventory();
+    }
 
-    const tableColumns = columns.map((info) => {
-        return (
-            <th>{info.title}</th>
-        )
+    openDetailsView = ((imdbID) => {
+        console.log("open: " + imdbID);
     });
 
-    const tableRows = data.map((info) => {
-        let st = IMDB_API_URL + info.imdbID;
-        return (
-            <tr>
-                <td><img alt="poster" src={info.Poster}></img></td>
-                <td>{info.Title}</td>
-                <td>{info.Year}</td>
-                <td><a href={st}>{info.imdbID}</a> </td>
-                <td>{info.Type}</td>
-            </tr>
-        )
-    });
+    render() {
 
-    return (
-        <div className="tableBlock">
-            <label>Title:</label>
-            <input placeholder="obligatory" type="text" id="textId"></input>
-            &nbsp;&nbsp;
-            <label>Year: </label>
-            <input placeholder="optional" type="text" id="yearId"></input>
-            &nbsp;&nbsp;
-            <button onClick={() => fetchInventory(1)}>Search</button>
-            <p></p>
-            <div id='resultSectionId' className="hideme">
-                <p>Found: {totalCount} results. &nbsp;&nbsp;
-                    <button onClick={() => fetchInventory(pageNumber - 1)}>Previous page</button> &nbsp;&nbsp;
-                    Displayed from {(pageNumber - 1) * 10 + 1} till {pageNumber * 10} &nbsp;&nbsp;
-                    <button onClick={() => fetchInventory(pageNumber + 1)}>Next Page</button></p>
-                <div>
-                    <table className="table table-stripped">
-                        <thead>
-                            <tr>{tableColumns}</tr>
-                        </thead>
-                        <tbody>{tableRows}</tbody>
-                    </table>
+        const columns = [
+            { name: 'Poster', title: 'Poster' },
+            { name: 'Title', title: 'Title' },
+            { name: 'Year', title: 'Year' },
+            { name: 'imdbID', title: 'imdbID' },
+            { name: 'Type', title: 'Type' }
+        ];
+        const tableColumns = columns.map((info) => <th key={info.title}>{info.title}</th>);
+
+        const tableRows = this.state.data.map((info) => {
+            let st = IMDB_API_URL + info.imdbID;
+            return (
+                <tr key={info.imdbID} onClick={() => this.openDetailsView(info.imdbID)}>
+                    <td><img alt="poster" src={info.Poster}></img></td>
+                    <td>{info.Title} <button onClick={() => this.openDetailsView(info.imdbID)}>Search</button></td>
+                    <td>{info.Year}</td>
+                    <td><a href={st}>{info.imdbID}</a> </td>
+                    <td>{info.Type}</td>
+                </tr>
+            )
+        });
+
+        return (
+            <div className="tableBlock" >
+                <label>Title:</label>
+                <input placeholder="obligatory" type="text" id="textId"></input>
+                &nbsp;&nbsp;
+                <label> Year: </label >
+                <input placeholder="optional" type="text" id="yearId"></input>
+                &nbsp;&nbsp;
+                <button onClick={() => this.goFirstPage()}>Search</button>
+                <p></p>
+                <div id='resultSectionId' className="hideme">
+                    <p>Found: {this.state.totalCount} results. &nbsp;&nbsp;
+                        <button onClick={() => this.goPrevPage()}>Previous page</button> &nbsp;&nbsp;
+                        Displayed from {(this.state.pageNumber - 1) * 10 + 1} till {this.state.pageNumber * 10} &nbsp;&nbsp;
+                        <button onClick={() => this.goNextPage()}>Next Page</button></p>
+                    <div>
+                        <table className="table table-stripped">
+                            <thead>
+                                <tr>{tableColumns}</tr>
+                            </thead>
+                            <tbody>{tableRows}</tbody>
+                        </table>
+                    </div>
+                    <p>Found: {this.state.totalCount} results. &nbsp;&nbsp;
+                        <button onClick={() => this.goPrevPage()}>Previous page</button> &nbsp;&nbsp;
+                        Displayed from {(this.state.pageNumber - 1) * 10 + 1} till {this.state.pageNumber * 10} &nbsp;&nbsp;
+                        <button onClick={() => this.goNextPage()}>Next Page</button></p>
                 </div>
-                <p>Found: {totalCount} results. &nbsp;&nbsp;
-                    <button onClick={() => fetchInventory(pageNumber - 1)}>Previous page</button> &nbsp;&nbsp;
-                    Displayed from {(pageNumber - 1) * 10 + 1} till {pageNumber * 10} &nbsp;&nbsp;
-                    <button onClick={() => fetchInventory(pageNumber + 1)}>Next Page</button></p>
-            </div>
-        </div>
-    );
+            </div >
+        );
+    }
 }
 
-export default App;
+export default HomeScreen;
