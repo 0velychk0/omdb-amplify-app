7391cfa811b527c1cb710f655c13529f1a9d1c90
Oleksandr_Velychko2 <Alex.Velichko@gmail.com>
2022-04-27 15:26:21 +0300

    Icon View Update

--- src/DetailsViewModule.jsx
+++ src/DetailsViewModule.jsx
@@ -1,36 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from "react-router-dom";
 
-// const API_HOST = "http://localhost:3000";
-// const INVENTORY_API_URL = `${API_HOST}/TestData/data.json`;
-// const IMDB_API_URL = "https://www.imdb.com/title/"
+const IMDB_API_URL = "https://www.imdb.com/title/"
 const INVENTORY_API_URL = "https://www.omdbapi.com/?apikey=1ac1214b"
 
 const defaultValue = {
-    Title: "",
-    Year: "",
-    Rated: "",
-    Released: "",
-    Runtime: "",
-    Poster: "",
+    "Title": "Live Free or Die Hard",
+    "Year": "2007",
+    "Rated": "PG-13",
+    "Released": "27 Jun 2007",
+    "Runtime": "128 min",
+    "Genre": "Action, Thriller",
+    "Director": "Len Wiseman",
+    "Writer": "John Carlin, Roderick Thorp, Mark Bomback",
+    "Actors": "Bruce Willis, Justin Long, Timothy Olyphant",
+    "Plot": "John McClane and a young hacker join forces to take down master cyber-terrorist Thomas Gabriel in Washington D.C.",
+    "Language": "English, Italian, French",
+    "Country": "United States, United Kingdom",
+    "Awards": "3 wins & 16 nominations",
+    "Poster": "n/a",
+    "Ratings": [
+        {
+            "Source": "Internet Movie Database",
+            "Value": "7.1/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "82%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "69/100"
+        }
+    ],
+    "Metascore": "69",
+    "imdbRating": "7.1",
+    "imdbVotes": "402,268",
+    "imdbID": "tt0337978",
+    "Type": "movie",
+    "DVD": "20 Nov 2007",
+    "BoxOffice": "$134,529,403",
+    "Production": "N/A",
+    "Website": "N/A",
+    "Response": "True"
 };
 
 function DetailsViewModule(props) {
 
-    // let navigate = useNavigate();
-
     const [data, setData] = useState(defaultValue);
     const location = useLocation();
-    let imdbId = '';
 
-    function fetchInventory() {
-        imdbId = (location != null && location.state != null && location.state.getImdbID != null)
-            ? location.state.getImdbID : "";
+    function fetchInventory(imdbId) {
+
         console.log(`fetchInventory called with imdbId: '${imdbId}'`);
 
         if (imdbId === "") {
             setData({});
-            // document.getElementById('resultSectionId').className = "hideme";
             return;
         }
         let uri = "&i=" + imdbId;
@@ -40,25 +65,46 @@ function DetailsViewModule(props) {
             .then(json => setData(json))
             .catch(e => {
                 console.error("error:" + e);
-                // document.getElementById('resultSectionId').className = "hideme";
                 this.setData(defaultValue);
             });
     }
 
     useEffect(() => {
         console.log('useEffect called');
-        fetchInventory();
-    }, []);
+
+        const params = new URLSearchParams(location.search);
+        const imdbID = params.get('imdbID');
+
+        fetchInventory(imdbID);
+    }, [location.search]);
+
+    const st = IMDB_API_URL + data.imdbID;
 
     return (
-        <div>
-            <img alt="poster" src={data.Poster}></img>
-            <li>Title: {data.Title}</li>
-            <li>Year: {data.Year}</li>
-            <li>Rated: {data.Rated}</li>
-            <li>Released: {data.Released}</li>
-            <li>Runtime: {data.Runtime}</li>
-        </div>
+        <table>
+            <tr>
+                <td><img alt="poster" src={data.Poster}></img></td>
+                <td>
+                    <li>Title: {data.Title}</li>
+                    <li>Year: {data.Year}</li>
+                    <li>Rated: {data.Rated}</li>
+                    <li>Released: {data.Released}</li>
+                    <li>Runtime: {data.Runtime}</li>
+                    <li>Genre: {data.Genre}</li>
+                    <li>Director: {data.Director}</li>
+                    <li>Writer: {data.Writer}</li>
+                    <li>Actors: {data.Actors}</li>
+                    <li>Plot: {data.Plot}</li>
+                    <li>Language: {data.Language}</li>
+                    <li>Country: {data.Country}</li>
+                    <li>Awards: {data.Awards}</li>
+                    <li>imdb rating: {data.imdbRating} votes: {data.imdbVotes}</li>
+                    <li><a href={st}>IMDB Link</a></li>
+                    <li>Type: {data.Type}</li>
+                    <li>BoxOffice: {data.BoxOffice}</li>
+                </td>
+            </tr>
+        </table>
     );
 }
 
--- src/IconsViewModule.jsx
+++ src/IconsViewModule.jsx
@@ -1,112 +1,197 @@
-import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
-// import React, { useEffect, useState } from 'react';
-// import { NavLink, Routes, Route, Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { createSearchParams, useNavigate, useLocation } from "react-router-dom";
 
-// const API_HOST = "http://localhost:3000";
-// const INVENTORY_API_URL = `${API_HOST}/TestData/data.json`;
-const IMDB_API_URL = "https://www.imdb.com/title/"
 const INVENTORY_API_URL = "https://www.omdbapi.com/?apikey=1ac1214b"
 
-let pageNumber = 1;
-let totalCount = 0;
-
 function IconsViewModule() {
-    const columns = [
-        { name: 'Poster', title: 'Poster' },
-        { name: 'Title', title: 'Title' },
-        { name: 'Year', title: 'Year' },
-        { name: 'imdbID', title: 'imdbID' },
-        { name: 'Type', title: 'Type' }
-    ];
 
+    const location = useLocation();
     const navigate = useNavigate();
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({
+        title: "",
+        year: "",
+        page: 1,
+        totalResults: 0,
+        result: []
+    });
+
+    useEffect(() => {
+        console.log('useEffect called');
+        const params = new URLSearchParams(location.search);
+        const searchTitle = params.get('title');
+        const searchYear = params.get('year');
+        const searchPage = params.get('page');
+
+        fetchInventory(searchTitle, searchYear, searchPage);
+    }, [location.search]);
 
     // GET request function to your Mock API
-    function fetchInventory(requiredPage) {
-        pageNumber = requiredPage;
-        if (pageNumber < 1) pageNumber = 1;
+    function fetchInventory(searchTitle, searchYear, searchPage) {
 
-        let textId = document.getElementById("textId").value.trim();
-        let yearId = document.getElementById("yearId").value.trim();
+        console.log(`fetchInventory called with text: '${searchTitle}', year: '${searchYear}', requiredPage: '${searchPage}'`);
 
-        console.log(`fetchInventory called with text: '${textId}', year: '${yearId}', requiredPage: '${requiredPage}'`);
-
-        if (textId === "") {
-            setData([]);
-            document.getElementById('resultSectionId').className = "hideme";
+        if (searchTitle == null || searchTitle === "") {
+            setData({
+                title: searchTitle,
+                year: searchYear,
+                page: searchPage,
+                totalResults: 0,
+                result: []
+            });
             return;
         }
-        let uri = "&s=" + textId;
-        if (yearId !== "") {
-            uri = uri + "&y=" + yearId;
+
+        let uri = "&s=" + searchTitle;
+        if (searchYear !== "") {
+            uri = uri + "&y=" + searchYear;
         }
-        if (pageNumber > 0) {
-            uri = uri + "&page=" + pageNumber;
+        if (searchPage > 0) {
+            uri = uri + "&page=" + searchPage;
         }
 
         fetch(`${INVENTORY_API_URL}${uri}`)
             .then(res => res.json())
             .then(json => {
-                totalCount = json.totalResults;
-                if (totalCount > 0)
-                    document.getElementById('resultSectionId').className = "";
-                else
-                    document.getElementById('resultSectionId').className = "hideme";
-                setData((json.Search != null) ? json.Search : [])
+                if (json.Response && json.Search != null) {
+                    setData({
+                        title: searchTitle,
+                        year: searchYear,
+                        page: searchPage,
+                        totalResults: json.totalResults,
+                        result: json.Search
+                    });
+                } else {
+                    setData({
+                        title: searchTitle,
+                        year: searchYear,
+                        page: searchPage,
+                        totalResults: 0,
+                        result: []
+                    });
+                }
             })
             .catch(e => {
                 console.error("error:" + e);
-                document.getElementById('resultSectionId').className = "hideme";
-                setData([]);
+                setData({
+                    title: searchTitle,
+                    year: searchYear,
+                    page: searchPage,
+                    totalResults: 0,
+                    result: []
+                });
             });
+    }
+
+    function updateCurrentView() {
+        let textId = document.getElementById("textId").value.trim();
+        let yearId = document.getElementById("yearId").value.trim();
+
+        navigate({
+            pathname: "/",
+            search: createSearchParams({
+                title: textId,
+                year: yearId,
+                page: 1
+            }).toString()
+        });
+        fetchInventory(textId, yearId, 1);
     }
 
     function openDetailsView(imdbID) {
         console.log("try to open: " + imdbID);
-        navigate("/detailsViewModule", { state: { getImdbID: imdbID } });
+
+        navigate({
+            pathname: "/detailsViewModule",
+            search: createSearchParams({
+                imdbID: imdbID
+            }).toString(),
+            refresh: true
+        });
     }
 
-    const tableColumns = columns.map((info) => {
+    function goNextPage() {
+        let textId = document.getElementById("textId").value.trim();
+        let yearId = document.getElementById("yearId").value.trim();
+        let nextPageNum = ((parseInt(data.page) * 10) > parseInt(data.totalResults)) ? parseInt(data.page) : (parseInt(data.page) + 1);
+
+        navigate({
+            pathname: "/",
+            search: createSearchParams({
+                title: textId,
+                year: yearId,
+                page: nextPageNum
+            }).toString()
+        });
+        fetchInventory(textId, yearId, nextPageNum);
+    }
+
+    function goPrevPage() {
+        let textId = document.getElementById("textId").value.trim();
+        let yearId = document.getElementById("yearId").value.trim();
+        let prevPageNum = (parseInt(data.page) > 1) ? (parseInt(data.page) - 1) : 1;
+
+        navigate({
+            pathname: "/",
+            search: createSearchParams({
+                title: textId,
+                year: yearId,
+                page: prevPageNum
+            }).toString()
+        });
+        fetchInventory(textId, yearId, prevPageNum);
+    }
+
+    const tableRows = data.result.map((info) => {
         return (
-            <th key={info.title}>{info.title}</th>
+            <div
+                key={info.imdbID}
+                className="icon-box-image"
+                onClick={() => openDetailsView(info.imdbID)}>
+
+                {(() => {
+                    if (info.Poster != null && info.Poster.startsWith('http')) {
+                        return (
+                            <img
+                                key={info.imdbID}
+                                alt="poster"
+                                className="icon-image"
+                                src={info.Poster}
+                            />
+                        )
+                    } else {
+                        return (
+                            <>
+                                <li>Title: {info.Title}</li>
+                                <li>Year: {info.Year}</li>
+                                <li>Type: {info.Type}</li>
+                            </>
+                        )
+                    }
+                })()}
+
+            </div>
         )
     });
 
-    const tableRows = data.map((info) => {
-        let st = IMDB_API_URL + info.imdbID;
-        return (
-            <>
-            <img alt="poster" src={info.Poster} key={info.imdbID} onClick={() => openDetailsView(info.imdbID)} /><wbr />
-            </>
-        )
-    });
-
-    let nextPage = (pageNumber * 10 < totalCount) ? pageNumber + 1 : pageNumber;
-    let prevPage = (pageNumber > 1) ? pageNumber - 1 : pageNumber;
     return (
         <div className="tableBlock">
             <label>Title:</label>
-            <input placeholder="obligatory" type="text" id="textId"></input>
+            <input placeholder="obligatory" type="text" id="textId" defaultValue={data.title}></input>
             &nbsp;&nbsp;
             <label>Year: </label>
-            <input placeholder="optional" type="text" id="yearId"></input>
+            <input placeholder="optional" type="text" id="yearId" defaultValue={data.year}></input>
             &nbsp;&nbsp;
-            <button onClick={() => fetchInventory(1)}>Search</button>
-            <p />
-            <div id='resultSectionId' className="hideme">
-                <p>Found: {totalCount} results. &nbsp;&nbsp;
-                    <button onClick={() => fetchInventory(prevPage)}>Previous page</button> &nbsp;&nbsp;
-                    Current page {pageNumber} &nbsp;&nbsp;
-                    <button onClick={() => fetchInventory(nextPage)}>Next Page</button></p>
-                <div> {tableRows} </div>
-                <hr />
-                <p>Found: {totalCount} results. &nbsp;&nbsp;
-                    <button onClick={() => fetchInventory(prevPage)}>Previous page</button> &nbsp;&nbsp;
-                    Current page {pageNumber} &nbsp;&nbsp;
-                    <button onClick={() => fetchInventory(nextPage)}>Next Page</button></p>
-            </div>
+            <button onClick={() => updateCurrentView()}>Search</button>
+            &nbsp;&nbsp;
+            Found: {data.totalResults} results.
+            &nbsp;&nbsp;
+            <button onClick={() => goPrevPage()}>Previous page</button>
+            &nbsp;&nbsp;
+            Current page {data.page}
+            &nbsp;&nbsp;
+            <button onClick={() => goNextPage()}>Next Page</button>
+            <hr />
+            <div id='resultSectionId'>{tableRows}</div>
         </div>
     );
 }
